7f5ad9b33581c95579245e37e4ece71f044e2f9a
sasi <bandisasidharreddy1210@gmail.com>
2024-10-25 13:28:00 +0530

    sasi

---
+++ src/sasidhar/images/sasi.js
@@ -0,0 +1,75 @@
+
+                        // {
+                        //     "id": 1,
+                        //     "title": "Personal Portfolio April Fools",
+                        //     "subtitle": "University of DVI (1997 - 2001)",
+                        //     "rating": "4.30/5",
+                        //     "description": "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                        // },
+                        // {
+                        //     "id": 2,
+                        //     "title": "Examples Of Personal Portfolio",
+                        //     "subtitle": "College of Studies (2000 - 2002)",
+                        //     "rating": "4.50/5",
+                        //     "description": "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                        // },
+                        // {
+                        //     "id": 3,
+                        //     "title": "Tips For Personal Portfolio",
+                        //     "subtitle": "University of Studies (1997 - 2001)",
+                        //     "rating": "4.80/5",
+                        //     "description": "If you are going to use a passage. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                        // }
+
+                        // {
+                        //     "id": 1,
+                        //     "title": "Diploma in Web Development",
+                        //     "subtitle": "BSE In CSE (2004 - 2008)",
+                        //     "rating": "4.70/5",
+                        //     "description": "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                        // },
+                        // {
+                        //     "id": 2,
+                        //     "title": "The Personal Portfolio Mystery",
+                        //     "subtitle": "Job at Rainbow-Themes (2008 - 2016)",
+                        //     "rating": "4.95/5",
+                        //     "description": "Generate Lorem Ipsum which looks. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                        // },
+                        // {
+                        //     "id": 3,
+                        //     "title": "Diploma in Computer Science",
+                        //     "subtitle": "Works at Plugin Development (2016 - 2020)",
+                        //     "rating": "5.00/5",
+                        //     "description": "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                        // }
+
+                        
+                //     "id": "default-home-experience-left",
+                //     "section_title": {
+                //         "title": "Company Experience",
+                //         "subtitle": "2007 - 2010"
+                //     },
+                //     "items": [
+                //         {
+                //             "id": 1,
+                //             "title": "Personal Portfolio April Fools",
+                //             "subtitle": "University of DVI (1997 - 2001)",
+                //             "rating": "4.30/5",
+                //             "description": "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                //         },
+                //         {
+                //             "id": 2,
+                //             "title": "Examples Of Personal Portfolio",
+                //             "subtitle": "College of Studies (2000 - 2002)",
+                //             "rating": "4.50/5",
+                //             "description": "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                //         },
+                //         {
+                //             "id": 3,
+                //             "title": "Tips For Personal Portfolio",
+                //             "subtitle": "University of Studies (1997 - 2001)",
+                //             "rating": "4.80/5",
+                //             "description": "If you are going to use a passage. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante."
+                //         }
+                //     ]
+                // },
